66a560fd1e25a207e0e5143be004e3bb35311e12
ZenoJha <friendashutosh.fa@gmail.com>
2024-07-01 16:23:07 +0545

    i think i fixed it

---
+++ Frontend/src/components/custom/Header.jsx
@@ -0,0 +1,30 @@
+import {} from 'react'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
+import { UserButton, useUser } from '@clerk/clerk-react'
+
+function Header() {
+    const { user, isSignedIn } = useUser();
+    return (
+        <div className='p-3 px-5 flex justify-between shadow-md'>
+        <Link to={'/dashboard'} className='flex items-center'>
+          <img src='/logo.svg' className='cursor-pointer' width={60} height={90} />
+          <p className='ml-2'>AI-Resume-Builder</p>
+        </Link>
+            {isSignedIn ?
+                <div className='flex gap-2 items-center'>
+                    <Link to={'/dashboard'}>
+                        <Button variant="outline">Dashboard</Button>
+                    </Link>
+                    <UserButton />
+                </div> :
+                <Link to={'/auth/sign-in'}>
+                    <Button>Get Started</Button>
+                </Link>
+            }
+
+        </div>
+    )
+}
+
+export default Header
